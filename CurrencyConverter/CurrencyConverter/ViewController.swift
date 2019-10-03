defe673e4894e9358d34b75edccddc4f9eeaac21
Rwoltx <richardx1016@gmail.com>
2019-10-02 22:49:47 -0400

    Swift Fundamentals III project complete

--- CurrencyConverter/CurrencyConverter/ViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -8,7 +8,23 @@
 
 import UIKit
 
+
+enum Currency {
+    case cad
+    case mxn
+}
+
+
+
+
 class ViewController: UIViewController {
+    
+    var currencyFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        return formatter
+    }()
+    var currency: Currency = .cad
     
     // MARK: - Outlets/Properties
     
@@ -23,17 +39,54 @@ class ViewController: UIViewController {
     // MARK: - Actions
     
     @IBAction func convertButtonTapped(_ sender: UIButton) {
+        guard let userInputString = fromCurrencyTextField.text else {return}
+        guard let userInput = Double(userInputString) else {
+            print("Incorrect input")
+            return
+        }
+        
+//        toCurrencyTextField.text = currencyFormatter.string(from: NSNumber(value: userInput))
+        toCurrencyTextField.text = String(convert(userInput))
+        
         
     }
     
     @IBAction func cadButtonTapped(_ sender: UIButton) {
+        cadButton.isSelected.toggle()
+        if cadButton.isSelected == true {
+            currency = .cad
+            toCurrencyLabel.text =  "Currency (CAD)"
+            if mxnButton.isSelected == true {
+                mxnButton.isSelected.toggle()
+            }
+        }
         
     }
     
     @IBAction func mxnButtonTapped(_ sender: UIButton) {
+        mxnButton.isSelected.toggle()
+        if mxnButton.isSelected == true{
+            
+            currency = .mxn
+            toCurrencyLabel.text =  "Currency (MXN)"
+            if cadButton.isSelected == true {
+                cadButton.isSelected.toggle()
+            }
+        }
+        
         
     }
     
     // MARK: - Helper Methods
+    func convert(_ dollars: Double) -> Double {
+        var convertedAmount: Double
+        switch currency {
+        case .cad:
+            convertedAmount = dollars * 1.33
+        case .mxn:
+            convertedAmount = dollars * 19.70
+        }
+        return convertedAmount
+    }
 }
 
